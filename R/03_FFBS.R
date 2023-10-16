69c934b8de48868e2e9ca2dd84f7a9a01ece18e9
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-10-16 10:24:06 +0200

    initial commit

---
+++ R/03_FFBS.R
@@ -0,0 +1,37 @@
+#' Title
+#'
+#' @param TT
+#' @param nfac
+#' @param Phi
+#' @param Q
+#' @param filt_f
+#' @param filt_P
+#'
+#' @return
+#' @export
+#'
+#' @examples
+GibbsSSM_f <- function(TT, nfac, Phi, Q, filt_f, filt_P) {
+  fTT <- filt_f[, TT]
+  PTT <- filt_P[, , TT]
+
+  fPost <- matrix(rep(0, nfac * TT), ncol = TT)
+  fPost[, TT] <- MASS::mvrnorm(n = 1, mu = fTT, Sigma = PTT)
+
+  for (t in (TT - 1):1) {
+    ftt <- filt_f[, t]
+    Ptt <- filt_P[, , t]
+    IS <- Ptt %*% t(Phi) %*% solve(Phi %*% Ptt %*% t(Phi) + Q)
+    sim_m <- ftt + IS %*% (fPost[, t + 1] - Phi %*% ftt)
+    sim_v <- Ptt - IS %*% Phi %*% Ptt
+    sim_v <- 0.5 * (sim_v + t(sim_v))
+    # if(!matrixcalc::is.positive.definite(sim_v)){
+    #   print(iter)
+    #   print(sim_v)
+    #   print(filt_P)
+    # }
+    fPost[, t] <- MASS::mvrnorm(n = 1, mu = sim_m, Sigma = sim_v)
+  }
+
+  return(fPost)
+}
---
+++ R/99_helper_gibbs_sampler_core.R
@@ -0,0 +1,162 @@
+sumffkronV <- function(availableObs, npara, nreg, njointfac, i, fPost, wReg, Viarray, type) {
+  summ <- 0
+  for (tt in availableObs) {
+    if (type == "allidio") {
+      if (njointfac != 0) {
+        f <- fPost[c(1:njointfac, (njointfac + 1 + npara * (i - 1)):(njointfac + npara * i)), tt] # generalize of no njointfac is included
+      } else {
+        f <- fPost[(njointfac + 1 + npara * (i - 1)):(njointfac + npara * i), tt]
+      }
+    } else if (type == "countryidio" | type == "countryidio_nomu") {
+      if (njointfac != 0) {
+        f <- fPost[c(1:njointfac, njointfac + i), tt] # generalize of no njointfac is included
+      } else {
+        f <- fPost[i, tt]
+      }
+    } else {
+      stop("No valid model type selected.")
+    }
+    
+    if (nreg != 0) {
+      wReg_it <- wReg[(1 + nreg * (i - 1)):(i * nreg), tt]
+      f <- c(f, wReg_it)
+    }
+    
+    
+    V <- Viarray[, , tt]
+    summ <- summ + kronecker(f %*% t(f), solve(V))
+  }
+  return(summ)
+}
+
+
+sumfyV <- function(availableObs, npara, nreg, njointfac, i, fPost, wReg, yiObs, Viarray, type) {
+  summ <- 0
+  for (tt in availableObs) {
+    if (type == "allidio") {
+      if (njointfac != 0) {
+        f <- fPost[c(1:njointfac, (njointfac + 1 + npara * (i - 1)):(njointfac + npara * i)), tt] # generalize of no njointfac is included
+      } else {
+        f <- fPost[(njointfac + 1 + npara * (i - 1)):(njointfac + npara * i), tt]
+      }
+    } else if (type == "countryidio" | type == "countryidio_nomu") {
+      if (njointfac != 0) {
+        f <- fPost[c(1:njointfac, njointfac + i), tt] # generalize of no njointfac is included
+      } else {
+        f <- fPost[i, tt]
+      }
+    } else {
+      stop("No valid model type selected.")
+    }
+    
+    if (nreg != 0) {
+      wReg_it <- wReg[(1 + nreg * (i - 1)):(i * nreg), tt]
+      f <- c(f, wReg_it)
+    }
+    
+    y <- yiObs[, tt]
+    V <- Viarray[, , tt]
+    summ <- summ + solve(V) %*% y %*% t(f)
+  }
+  return(summ)
+}
+
+matSqrt <- function(V) {
+  npara <- dim(V)[1]
+  if (any(is.na(V))) {
+    matrix(NA, nrow = npara, ncol = npara)
+  } else {
+    eig <- eigen(V)
+    val <- eig$values
+    vec <- eig$vectors
+    # vec %*% diag(sqrt(val)) %*% t(vec)
+    vec %*% diag(sqrt(val))
+  }
+}
+
+bdiagByTime <- function(VhatArray_A, npara, N, TT, Nnpara) {
+  VhatList <- plyr::alply(VhatArray_A, 3)
+  VhatList_split <- split(VhatList, rep(1:TT, N))
+  VhatList_bdiag <- lapply(VhatList_split, Matrix::bdiag)
+  VhatArray_bdiag <- array(unlist(lapply((VhatList_bdiag), as.matrix)), c(Nnpara, Nnpara, TT))
+  
+  return(VhatArray_bdiag)
+}
+
+
+
+utuSum <- function(uSplit, VhatSqrt, TT, N, npara) {
+  sumUtu <- 0
+  utildeSplit <- lapply(1:N, matrix, data = NA, nrow = npara, ncol = TT)
+  sumUtu_individ <- lapply(1:N, matrix, data = NA, nrow = npara, ncol = npara)
+  for (i in 1:N) {
+    for (t in 1:TT) {
+      utildeSplit[[i]][, t] <- solve(VhatSqrt[, , (i - 1) * TT + t]) %*% uSplit[[i]][, t]
+    }
+    
+    utu <- apply(utildeSplit[[i]], 2, function(x) {
+      x %*% t(x)
+    })
+    sumUtu_i <- apply(utu, 1, sum, na.rm = TRUE)
+    sumUtu <- sumUtu + sumUtu_i
+    sumUtu_i_mat <- matrix(sumUtu_i, ncol = npara)
+    sumUtu_individ[[i]] <- (t(sumUtu_i_mat) + sumUtu_i_mat) / 2
+  }
+  return(list(sumUtu_total = matrix(sumUtu, ncol = npara), sumUtu_individ = sumUtu_individ))
+}
+
+
+
+
+makeBstart <- function(npara, N, p, p_joint, B_par, type = "allidio") { # type = c("allidio","countryidio")
+  
+  # if(length(B_par) == 1){B_par <- rep(B_par,N*npara*(p_joint+1))}
+  B_stack <- matrix(rep(0, N * npara * p), ncol = p)
+  
+  if (type == "allidio") {
+    non_zeros <- cbind(matrix(TRUE, ncol = p_joint, nrow = N * npara), diag(rep(TRUE, N * npara)))
+    B_stack[non_zeros] <- B_par
+  } else if (type == "countryidio") {
+    non_zeros <- cbind(matrix(TRUE, ncol = p_joint, nrow = N * npara), ifelse(kronecker(diag(N), rep(1, npara)) == 1, TRUE, FALSE))
+    B_stack[non_zeros] <- B_par
+  } else if (type == "countryidio_nomu") {
+    non_zeros <- cbind(matrix(TRUE, ncol = p_joint, nrow = N * npara), ifelse(kronecker(diag(N), c(rep(1, npara - 1), 0)) == 1, TRUE, FALSE))
+    B_stack[non_zeros] <- B_par
+  } else {
+    stop("No valid model type selected.")
+  }
+  B_i <- makeBi(npara, N, p, p_joint, B_stack, type = type)
+  
+  return(list(B_stack = B_stack, B_i = B_i))
+}
+
+makeBi <- function(npara, N, p, p_joint, B_stack, type = "allidio") { # type = c("allidio","countryidio")
+  if (type == "allidio") {
+    if (sum(B_stack) == 0) {
+      B_i <- array(B_stack, dim = c(npara, npara + p_joint, N))
+    } else {
+      B <- aperm(array(c(t(B_stack)), dim = c(p_joint + npara * N, npara, N)), perm = c(2, 1, 3))
+      B_i <- array(dim = c(npara, p_joint + npara, N))
+      for (i in 1:N) {
+        B_i[, , i] <- B[, colSums(B[, , i]) != 0, i]
+      }
+    }
+  } else if (type == "countryidio" | type == "countryidio_nomu") {
+    B <- aperm(array(c(t(B_stack)), dim = c(p_joint + N, npara, N)), perm = c(2, 1, 3))
+    B_i <- array(dim = c(npara, p_joint + 1, N))
+    for (i in 1:N) {
+      B_i[, , i] <- B[, colSums(B[, , i]) != 0, i]
+    }
+  } else {
+    stop("No valid model type selected.")
+  }
+  return(B_i = B_i)
+}
+
+
+makeDstart <- function(npara, N, nreg, D_par) {
+  Dmat <- matrix(D_par, nrow = npara, ncol = nreg)
+  Dstack <- kronecker(diag(N), Dmat)
+  D_i <- array(D_par, c(npara, nreg, N))
+  return(list(Dstack = Dstack, D_i = D_i))
+}
