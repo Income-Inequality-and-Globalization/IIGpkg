4fb178be6ce272f66b8367ffb7c72ae8b3f9667c
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-01-23 17:19:08 +0100

    feat: update container generator to encompass all MCMC output containers

--- R/42_helper_full_measures_cnt.R
+++ R/42_helper_full_measures_cnt.R
@@ -1,3 +1,14 @@
+get_cnt_me_B <- function(out_B,
+                         NN = 10,
+                         num_pars = 3,
+                         names_para = c("a", "b", "mu")) {
+  nms_rw <- get_cnt_names_NN(names_para, num_pars, NN)
+  nms_cl <- get_cnt_names_facs(names_para, num_pars, NN)
+  nms_mm <- get_cnt_names_dimMM(dim(out_B)[3])
+
+  dimnames(out_B) <- list(nms_rw, nms_cl, nms_mm)
+  return(out_B)
+}
 get_cnt_me_f <- function(out_f,
                          NN = 10,
                          num_pars = 3,
@@ -8,6 +19,28 @@ get_cnt_me_f <- function(out_f,
 
   dimnames(out_f) <- list(nms_rw, nms_cl, nms_mm)
   return(out_f)
+}
+get_cnt_me_D <- function(out_D,
+                         NN = 10,
+                         num_pars = 3,
+                         names_para = c("a", "b", "mu")) {
+  nms_rw <- get_cnt_names_NN(names_para, num_pars, NN)
+  nms_cl <- get_cnt_names_NN(names_para, num_pars, NN)
+  nms_mm <- get_cnt_names_dimMM(dim(out_D)[3])
+
+  dimnames(out_D) <- list(nms_rw, nms_cl, nms_mm)
+  return(out_D)
+}
+get_cnt_me_wRegs <- function(out_wRegs,
+                             NN = 10,
+                             num_pars = 3,
+                             names_para = c("a", "b", "mu")) {
+  nms_rw <- get_cnt_names_NN(names_para, num_pars, NN)
+  nms_cl <- get_cnt_names_TT(ncol(out_wRegs))
+
+  rownames(out_wRegs) <- nms_rw
+  colnames(out_wRegs) <- nms_cl
+  return(out_wRegs)
 }
 get_cnt_names_facs <- function(nms_pars, num_pars, NN) {
   seq_NN <- get_cnt_seq_NN(num_pars, NN)
