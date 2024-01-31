dfeaf820c26dd4ff543329d0ef8d9dc82ccc9bf0
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-01-31 10:24:14 +0100

    feat: add support for grid values in gini-computation

--- R/42_measures_gini.R
+++ R/42_measures_gini.R
@@ -1,22 +1,51 @@
 compute_gini_info <- function(out_a, out_q, ki_prob = 0.95) {
   NN <- dim(out_a)[1]
   TT <- dim(out_a)[2]
-
-  ki_ints <- compute_ki_upper_lower(ki_prob = ki_prob)
-  ki_upp <- ki_ints[1]
-  ki_low <- ki_ints[2]
-  out_gini <- array(0, dim = c(NN, TT, 3))
-  for (nn in seq_len(NN)) {
-    for (tt in seq_len(TT)) {
-      vec_tmp <- compute_gini_sm(out_a[nn, tt, ], out_q[nn, tt, ])
-      out_gini[nn, tt, 1]  <- mean(vec_tmp)
-      out_gini[nn, tt, c(2, 3)] <- quantile(vec_tmp, probs = c(ki_upp, ki_low))
-    }
+  if (length(dim(out_a)) > 3) {
+    GG <- dim(out_a)[3]
+  } else {
+    GG <- NULL
   }
-  new_rn <- sub("^a_", "", rownames(out_a))
-  dimnames(out_gini) <- list(new_rn,
-                             colnames(out_a),
-                             c("mean", "ki_upp", "ki_low"))
+
+  ki_int <- compute_ki_upper_lower(ki_prob = ki_prob)
+  ki_upp <- ki_int[1]
+  ki_low <- ki_int[2]
+
+  out_gini <- compute_gini(out_a, out_q, NN, TT, GG, ki_upp, ki_low)
+  return(out_gini)
+}
+compute_gini <- function(out_a, out_q, NN, TT, GG, ki_upp, ki_low) {
+  if (is.null(GG)) {
+    out_gini <- array(0, dim = c(NN, TT, 3))
+    for (nn in seq_len(NN)) {
+      for (tt in seq_len(TT)) {
+        vec_tmp <- compute_gini_sm(out_a[nn, tt, ], out_q[nn, tt, ])
+        out_gini[nn, tt, 1]  <- mean(vec_tmp)
+        browser()
+        out_gini[nn, tt, c(2, 3)] <- quantile(vec_tmp, probs = c(ki_upp, ki_low))
+      }
+    }
+    new_rn <- sub("^a_", "", rownames(out_a))
+    dimnames(out_gini) <- list(new_rn,
+                               colnames(out_a),
+                               c("mean", "ki_upp", "ki_low"))
+  } else {
+    out_gini <- array(0, dim = c(NN, TT, GG, 3))
+    for (gg in seq_len(GG)) {
+      for (nn in seq_len(NN)) {
+        for (tt in seq_len(TT)) {
+          vec_tmp <- compute_gini_sm(out_a[nn, tt, gg, ], out_q[nn, tt, gg, ])
+          out_gini[nn, tt, gg, 1]  <- mean(vec_tmp)
+          out_gini[nn, tt, gg, c(2, 3)] <- quantile(vec_tmp, probs = c(ki_upp, ki_low))
+        }
+      }
+    }
+    new_rn <- sub("^a_", "", rownames(out_a))
+    dimnames(out_gini) <- list(new_rn,
+                               colnames(out_a),
+                               dimnames(out_a)[[3]],
+                               c("mean", "ki_upp", "ki_low"))
+  }
   return(out_gini)
 }
 compute_gini_sm <- function(a, q, logarithm = FALSE) {
