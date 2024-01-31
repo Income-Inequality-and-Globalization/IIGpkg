7217c22286e3dad70b1f91fb6978f730dd6e3254
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-01-31 10:19:47 +0100

    feat: update marginal effects container generator with grid dimension

--- R/42_helper_measures_cnt.R
+++ R/42_helper_measures_cnt.R
@@ -9,15 +9,18 @@ get_cnt_par_post_estim <- function(NN,
   return(out_post_estim)
 }
 get_cnt_par_me_estim <- function(NN,
+                                 TT,
+                                 GG,
+                                 MM,
                                  names_regs = NULL,
-                                 TT, MM,
                                  num_pars = 3,
                                  names_para = c("a", "q", "mu")) {
   if (is.null(names_regs)) stop("Arg. 'names_regs' missing.")
   KK <- length(names_regs)
-  out_post_estim <- array(0, dim = c(NN * num_pars, TT, MM, KK))
+  out_post_estim <- array(0, dim = c(NN * num_pars, TT, GG, MM, KK))
   dimnames(out_post_estim) <- list(get_cnt_names_NN(names_para, num_pars, NN),
                                    get_cnt_names_TT(TT),
+                                   get_cnt_names_GG(GG),
                                    get_cnt_names_dimMM(MM),
                                    names_regs)
   return(out_post_estim)
