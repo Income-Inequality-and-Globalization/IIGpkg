484b21551478b1388f2839316ae87fb2cfdb6687
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-02-06 09:34:57 +0100

    feat: update Gini/expectation main plot script

--- inst/result-plots/01_plots_ginis.R
+++ inst/result-plots/01_plots_ginis.R
@@ -6,15 +6,93 @@ library(IIGpkg)
 # Main execution block
 pth_dta <- file.path("./tests/testthat", "_data_test_measures")
 test_data <- readRDS(file.path(pth_dta, "output_gibbs_small.rds"))
-test_out <- test_data$out_gibbs
+test_out  <- test_data$out_gibbs
 y_standardization  <- test_data$yObs_standardization
 y_centered_values  <- test_data$yObs_centered_values
-transformation_infos <- list(centering = y_centered_values,
-                             scaling = y_standardization,
-                             names_regs = c("cpi_change",
-                                            "unemployment",
-                                            "gdp_ppp"))
+transformation_infos <- list(
+  y_settings = list(
+    centering = y_centered_values,
+    scaling = y_standardization),
+  x_settings = list(
+    grid_length = 100,
+    cut_off_num = 4,
+    names_regs = c("cpi_change",
+                   "unemployment",
+                   "gdp_ppp")
+  )
+)
 out_measures <- generate_measures_me(test_out, transformation_infos)
+
+# out_measures$gini_info_KK$cpi_change <- out_measures$gini_info_KK$cpi_change[, , 2, ]
+# out_measures$gini_info_KK$unemployment <- out_measures$gini_info_KK$unemployment[, , 2, ]
+# out_measures$gini_info_KK$gdp_ppp <- out_measures$gini_info_KK$gdp_ppp[, , 2, ]
+# out_measures$regressor_grid_transformed <- out_measures$regressor_grid_transformed[,, 2,]
+
+var_tkn <- "unemployment"
+par(mfrow = c(3, 3))
+nn_tk <- 1
+for (nn_tk in 1:9) {
+    # tmp_test <- apply(out_measures$gini_info_KK$unemployment[, , ,], c(1, 3, 4), mean)
+    #   testme_mean <- tmp_test[nn_tk, , 1]
+    # testme_kiup <- tmp_test[nn_tk, , 2]
+    # testme_kilo <- tmp_test[nn_tk, , 3]
+    # 
+    # min_y <- min(tmp_test[nn_tk, , ])
+    # max_y <- max(tmp_test[nn_tk, , ])
+    # plot(testme_mean, type = "l", ylim = c(min_y, max_y))
+    # lines(testme_kiup, col = "blue")
+    # lines(testme_kilo, col = "blue")
+  tmp_test <- out_measures$gini_info_KK[[var_tkn]][, , ,]
+  min_y <- min(tmp_test[nn_tk, , , 1])
+  max_y <- max(tmp_test[nn_tk, , , 1])
+  plot(tmp_test[nn_tk, 1, , 1], type = "l", ylim = c(min_y, max_y))
+  for (tt in 2:21) {
+    testme_mean <- tmp_test[nn_tk, tt, ,1]
+    lines(testme_mean)
+    # testme_kiup <- tmp_test[nn_tk, tt, ,2]
+    # testme_kilo <- tmp_test[nn_tk, tt, ,3]
+    # 
+    # 
+    # plot(testme_mean, type = "l", ylim = c(min_y, max_y))
+    # lines(testme_kiup, col = "b")
+    # lines(testme_kilo, col = "blue")
+  }
+}
+
+par(mfrow = c(3, 3))
+nn_tk <- 1
+for (nn_tk in 1:9) {
+  tmp_test <- apply(out_measures$gini_info_KK[[var_tkn]][, , ,], c(1, 3, 4), mean)
+    testme_mean <- tmp_test[nn_tk, , 1]
+  testme_kiup <- tmp_test[nn_tk, , 2]
+  testme_kilo <- tmp_test[nn_tk, , 3]
+
+  min_y <- min(tmp_test[nn_tk, , ])
+  max_y <- max(tmp_test[nn_tk, , ])
+  plot(testme_mean, type = "l") #, ylim = c(min_y, max_y))
+  # lines(testme_kiup, col = "blue"#)
+  #lines(testme_kilo, col = "blue")
+}
+
+# min_y <- min(out_measures$gini_info_KK$gdp_ppp[nn_tk, tt_tk, , ])
+# max_y <- max(out_measures$gini_info_KK$gdp_ppp[nn_tk, tt_tk, , ])
+plot(testme_mean, type = "l") #, ylim = c(min_y, max_y))
+
+nn_tk <- 1
+par(mfrow = c(4, 5))
+for (tt_tk in 1:20) {
+  testme_mean <- out_measures$gini_info_KK$gdp_ppp[nn_tk, tt_tk, , 1]
+  testme_kiup <- out_measures$gini_info_KK$gdp_ppp[nn_tk, tt_tk, , 2]
+  testme_kilo <- out_measures$gini_info_KK$gdp_ppp[nn_tk, tt_tk, , 3]
+  
+  # min_y <- min(out_measures$gini_info_KK$gdp_ppp[nn_tk, tt_tk, , ])
+  # max_y <- max(out_measures$gini_info_KK$gdp_ppp[nn_tk, tt_tk, , ])
+  plot(testme_mean, type = "l") #, ylim = c(min_y, max_y))
+  # lines(testme_kiup, col = "blue")
+  # lines(testme_kilo, col = "blue")
+}
+
+
 out_ginis_TT <- out_measures$gini_info_TT
 out_ginis_KK <- out_measures$gini_info_KK
 out_mus_TT   <- out_measures$mu_info_TT
