a540314dd2731973542fd0ac37c18aaa6011a709
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-02-26 13:45:17 +0100

    style: fix names of helper functions that name dimension of containers

--- R/42_helper_measures_computations.R
+++ R/42_helper_measures_computations.R
@@ -4,10 +4,10 @@ compute_ki_upper_lower <- function(ki_prob) {
   ki_lower <- (1 - ki_prob) / 2
   return(c(ki_upper, ki_lower))
 }
-get_id_D_uncertain <- function(all_names_Dcol, name_reg) {
+get_col_reg_name <- function(all_names_Dcol, name_reg) {
   which(grepl(name_reg, all_names_Dcol))
 }
-get_D_uncertain <- function(D, D_avg, mm, id_cu) {
-  D_avg[, id_cu, mm] <- D[, id_cu, mm]
-  return(D_avg[, , mm])
+get_D_uncertain <- function(D, D_avg, id_cu) {
+  D_avg[, id_cu] <- D[, id_cu]
+  return(D_avg)
 }
