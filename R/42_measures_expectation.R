b72076d8be936f02d0b1a5dc2ac11cdca00ab0b7
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-02-28 15:47:04 +0100

    feat: cleaner function bodies for mu computations
    - support for better container dimnames

--- R/42_measures_expectation.R
+++ R/42_measures_expectation.R
@@ -6,39 +6,38 @@ compute_mu_info <- function(mu_array, ki_prob = 0.95) {
   } else {
     GG <- NULL
   }
-  ki_int <- compute_ki_upper_lower(ki_prob = ki_prob)
-  ki_upp <- ki_int[1]
-  ki_low <- ki_int[2]
-  out_mu <- compute_mu(mu_array, NN, TT, GG, ki_upp, ki_low)
+  out_mu <- compute_mu(mu_array, NN, TT, GG, ki_prob = ki_prob)
   return(out_mu)
 }
-compute_mu <- function(mu, NN, TT, GG = NULL, ki_upp, ki_low) {
+compute_mu <- function(mu, NN, TT, GG = NULL, ki_prob = ki_prob) {
     if (is.null(GG)) {
       out_mu <- array(0, dim = c(NN, TT, 3))
       for (nn in seq_len(NN)) {
         for (tt in seq_len(TT)) {
-          vec_tmp <- mu[nn, tt, ]
+          vec_tmp <- get_vec_tmp(mu[nn, tt, ], rnd = 1)
           out_mu[nn, tt, 1]  <- mean(vec_tmp)
-          out_mu[nn, tt, c(2, 3)] <- quantile(vec_tmp,
-                                              probs = c(ki_upp, ki_low))
+          out_mu[nn, tt, c(2, 3)] <- quantile(vec_tmp, probs = ki_prob)
+          if (any(is.na(out_mu))) browser()
         }
       }
-      dimnames(out_mu) <- list(rownames(mu),
+      new_rn <- get_name_rows_measure_out(rownames(mu), "_mu")
+      dimnames(out_mu) <- list(new_rn,
                                colnames(mu),
                                c("mean", "ki_upp", "ki_low"))
     } else {
       out_mu <- array(0, dim = c(NN, TT, GG, 3))
-      for (gg in seq_len(GG)) {
-        for (nn in seq_len(NN)) {
-          for (tt in seq_len(TT)) {
-            vec_tmp <- mu[nn, tt, gg, ]
+      for (nn in seq_len(NN)) {
+        for (tt in seq_len(TT)) {
+          for (gg in seq_len(GG)) {
+            vec_tmp <- get_vec_tmp(mu[nn, tt, gg, ])
             out_mu[nn, tt, gg, 1]  <- mean(vec_tmp)
-            out_mu[nn, tt, gg, c(2, 3)] <- quantile(vec_tmp,
-                                                    probs = c(ki_upp, ki_low))
+            out_mu[nn, tt, gg, c(2, 3)] <- quantile(vec_tmp, probs = ki_prob)
+          if (any(is.na(out_mu))) browser()
           }
         }
       }
-      dimnames(out_mu) <- list(rownames(mu),
+      new_rn <- get_name_rows_measure_out(rownames(mu), "_mu")
+      dimnames(out_mu) <- list(new_rn,
                                colnames(mu),
                                dimnames(mu)[[3]],
                                c("mean", "ki_upp", "ki_low"))
