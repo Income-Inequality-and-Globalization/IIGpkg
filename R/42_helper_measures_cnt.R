f298f1262f87ea10bd38f09d44ff4c9ed57547b4
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-03-10 08:56:11 +0100

    feat: add helper for other storage container generation

--- R/42_helper_measures_cnt.R
+++ R/42_helper_measures_cnt.R
@@ -40,6 +40,21 @@ get_cnt_par_marg_estim <- function(NN,
                                    names_regs)
   return(out_post_estim)
 }
+get_cnt_par_me_raws <- function(NN,
+                                TT,
+                                MM,
+                                names_regs = NULL,
+                                num_pars = 3,
+                                names_para = c("a", "q", "mu")) {
+  if (is.null(names_regs)) stop("Arg. 'names_regs' missing.")
+  KK <- length(names_regs)
+  out_post_estim <- array(0, dim = c(NN * num_pars, TT, MM, KK))
+  dimnames(out_post_estim) <- list(get_cnt_names_NN(names_para, num_pars, NN),
+                                   get_cnt_names_TT(TT),
+                                   get_cnt_names_dimMM(MM),
+                                   names_regs)
+  return(out_post_estim)
+}
 get_cnt_me_B <- function(out_B,
                          NN = 10,
                          num_pars = 3,
