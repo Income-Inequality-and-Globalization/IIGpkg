66732c3b877f715e1e54b584a290d35625bc9530
IlyaZar <zarubin@wiso.uni-koeln.de>
2024-03-12 13:38:12 +0100

    feat: helper to compute mu for forward diffs

--- R/42_measures_expectation.R
+++ R/42_measures_expectation.R
@@ -9,7 +9,14 @@ compute_mu_info <- function(mu_array, ki_prob = 0.95) {
   out_mu <- compute_mu(mu_array, NN, TT, GG, ki_prob = ki_prob)
   return(out_mu)
 }
-compute_mu <- function(mu, NN, TT, GG = NULL, ki_prob = ki_prob) {
+compute_mu_info_fd <- function(mu_array_fd, mu_array_tt, ki_prob = 0.95) {
+  NN <- dim(mu_array_fd)[1]
+  TT <- dim(mu_array_fd)[2]
+
+  out_mu <- compute_mu_fd(mu_array_fd, mu_array_tt, NN, TT, ki_prob = ki_prob)
+  return(out_mu)
+}
+compute_mu <- function(mu, NN, TT, GG = NULL, ki_prob) {
     if (is.null(GG)) {
       out_mu <- array(0, dim = c(NN, TT, 3))
       for (nn in seq_len(NN)) {
@@ -29,7 +36,7 @@ compute_mu <- function(mu, NN, TT, GG = NULL, ki_prob = ki_prob) {
       for (nn in seq_len(NN)) {
         for (tt in seq_len(TT)) {
           for (gg in seq_len(GG)) {
-            vec_tmp <- get_vec_tmp(mu[nn, tt, gg, ])
+            vec_tmp <- get_vec_tmp(mu[nn, tt, gg, ], rnd = 0.7)
             out_mu[nn, tt, gg, 1]  <- mean(vec_tmp)
             out_mu[nn, tt, gg, c(2, 3)] <- quantile(vec_tmp, probs = ki_prob)
           if (any(is.na(out_mu))) browser()
@@ -42,5 +49,26 @@ compute_mu <- function(mu, NN, TT, GG = NULL, ki_prob = ki_prob) {
                                dimnames(mu)[[3]],
                                c("mean", "ki_upp", "ki_low"))
     }
+  return(out_mu)
+}
+compute_mu_fd <- function(mu_fd, mu_tt, NN, TT, ki_prob) {
+  out_mu <- array(0, dim = c(NN, TT, 3))
+  for (nn in seq_len(NN)) {
+    for (tt in seq_len(TT)) {
+      vec_tmp_fd <- get_vec_tmp(mu_fd[nn, tt, ])
+      vec_tmp_tt <- get_vec_tmp(mu_tt[nn, tt, ])
+
+      if (length(vec_tmp_fd) != length(vec_tmp_tt)) browser()
+      vec_tmp <- vec_tmp_fd - vec_tmp_tt
+
+      out_mu[nn, tt, 1]  <- mean(vec_tmp)
+      out_mu[nn, tt, c(2, 3)] <- quantile(vec_tmp, probs = ki_prob)
+      if (any(is.na(out_mu))) browser()
+    }
+  }
+  new_rn <- get_name_rows_measure_out(rownames(mu_fd), "_mu")
+  dimnames(out_mu) <- list(new_rn,
+                           colnames(mu_fd),
+                           c("mean", "ki_upp", "ki_low"))
   return(out_mu)
 }
